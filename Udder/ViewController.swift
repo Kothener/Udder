c2c2ee95d275a1964f8b1bdabcf95424fd9f6081
Franco <francomelendez@icloud.com>
2022-08-21 07:11:08 -0600

    adding

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-//hola
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //
 }
-
+}
